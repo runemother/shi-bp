ffa1d1640380344c585c051d032aea8803040195
Kihwa Hong <26red@hanmail.net>
2021-09-17 11:21:34 +0900

    Initial commit

---
+++ src/main/java/com/shi/bp/ShiBpApplication.java
@@ -0,0 +1,13 @@
+package com.shi.bp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiBpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiBpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shi/bp/ShiBpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shi.bp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiBpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
